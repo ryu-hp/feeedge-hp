2fbef53ae12c0bf7459b775748788021cf28a556
ryu-hp <ryupon34689@gmail.com>
2026-04-04 17:48:25 +0900

    Add logo icon with text as a PNG image

--- header.php
+++ header.php
@@ -1,8 +1,37 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+  <!-- Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-SRFS3H1TJG"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-SRFS3H1TJG');
+  </script>
+  <!-- End Google Analytics -->
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title> -->
+  <title>FREEDGEコーポレートサイト</title>
+  <meta name="description" content="デジタルでひろがる、自由な未来">
+  <meta name="keywords" content="FREEDGE,エンジニア,IT,情報,システム,自由,本気">
+  
+  <!-- OGP設定 -->
+  <meta property="og:title" content="FREEDGEコーポレートサイト">
+  <meta property="og:description" content="デジタルでひろがる、自由な未来">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://www.freedge.co.jp">
+  <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/image/logo-squeare.webp">
+  <meta property="og:site_name" content="FREEDGEコーポレートサイト">
+  <meta property="og:locale" content="ja_JP">
+  
+  <!-- Twitter Card -->
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:title" content="FREEDGEコーポレートサイト">
+  <meta name="twitter:description" content="デジタルでひろがる、自由な未来">
+  <meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/image/logo-squeare.webp">
+
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Bebas+Neue&family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
